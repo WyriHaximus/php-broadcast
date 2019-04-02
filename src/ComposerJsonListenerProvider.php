b8e004852932213ee5d2ea57d372b3ad5723f481
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-04-02 22:48:33 +0200

    Added missing full path resolving

--- src/ComposerJsonListenerProvider.php
+++ src/ComposerJsonListenerProvider.php
@@ -2,9 +2,10 @@
 
 namespace WyriHaximus\Broadcast;
 
+use Composed\AbstractPackage;
 use Psr\Container\ContainerInterface;
 use Psr\EventDispatcher\ListenerProviderInterface;
-use function WyriHaximus\from_get_in_packages_composer;
+use function WyriHaximus\get_in_packages_composer;
 use function WyriHaximus\iteratorOrArrayToArray;
 use function WyriHaximus\listClassesInFile;
 
@@ -18,7 +19,7 @@ final class ComposerJsonListenerProvider implements ListenerProviderInterface
     public function __construct(string $path, ContainerInterface $container)
     {
         $this->container = $container;
-        $this->events = iteratorOrArrayToArray($this->locateEvents(from_get_in_packages_composer($path)));
+        $this->events = iteratorOrArrayToArray($this->iteratePackages(get_in_packages_composer($path)));
     }
 
     public function getListenersForEvent(object $event): iterable
@@ -34,28 +35,35 @@ final class ComposerJsonListenerProvider implements ListenerProviderInterface
         }
     }
 
-    private function locateEvents(iterable $events): iterable
+    private function iteratePackages(iterable $packages): iterable
+    {
+        foreach ($packages as $package => $events) {
+            yield from $this->locateEvents($package, $events);
+        }
+    }
+
+    private function locateEvents(AbstractPackage $package, iterable $events): iterable
     {
         foreach ($events as $event => $listeners) {
-            yield $event => $this->locateListeners($listeners);
+            yield $event => $this->locateListeners($package, $listeners);
         }
     }
 
-    private function locateListeners(iterable $paths): iterable
+    private function locateListeners(AbstractPackage $package, iterable $paths): iterable
     {
         foreach ($paths as $listenerPaths) {
-            yield from $this->listListenersInLocation($listenerPaths);
+            yield from $this->listListenersInLocation($package, $listenerPaths);
         }
     }
 
-    private function listListenersInLocation(string $location): iterable
+    private function listListenersInLocation(AbstractPackage $package, string $location): iterable
     {
         if (\strpos($location, '*') !== false) {
-            yield from $this->locateListeners(\glob($location));
+            yield from $this->locateListeners($package, \glob($location));
 
             return;
         }
 
-        yield from listClassesInFile($location);
+        yield from listClassesInFile($package->getPath($location));
     }
 }
